ad1c516c2b973fdd5795470e7c9e02c7fb460128
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-02 21:08:34 +0900

    スプレッド構文確認

--- src/index.js
+++ src/index.js
@@ -127,5 +127,42 @@ sayHello();
 
 const sayHello2 = (name = "ゲスト") => console.log(`こんにちは！${name}さん`);
 sayHello2("aki");
-//引数がないとundefinedで出力される。
+//デフォルト値を引数に設定しているので"ゲストが出力される"
 sayHello2();
+
+/**
+ * スプレッド構文　...
+ */
+//配列の展開
+const arr1 = [1, 2];
+console.log(arr1);
+console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+sumFunc(arr1[0], arr1[1]);
+sumFunc(...arr1);
+
+// まとめる
+const arr2 = [1, 2, 3, 4, 5];
+const [num1, num2, ...arr3] = arr2;
+console.log(num1);
+console.log(num2);
+console.log(arr3);
+
+// 配列のコピー、結合
+const arr4 = [10, 20];
+const arr5 = [30, 40];
+
+//コピー
+const arr6 = [...arr4];
+console.log(arr6);
+const arr7 = [...arr4, ...arr5];
+console.log(arr7);
+
+//　普通に代入したら？
+// =で代入された値は参照渡しとなっているので、
+// arr8を変更するとarr4にも変更が適用されてしまう。
+const arr8 = arr4;
+arr8[0] = 100;
+console.log(arr8);
+console.log(arr4);
