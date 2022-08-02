5166587cfd17cd5a3adcd072db6dfe92e789a684
akinus <42932934+akimo10@users.noreply.github.com>
2022-08-02 19:17:13 +0900

    テンプレート文字列確認

--- src/index.js
+++ src/index.js
@@ -48,3 +48,18 @@ const val5 = ["dog", "cat"];
 val5[0] = "bird";
 val5.push("monkey");
 console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+const name = "aki";
+const age = 30;
+//「私の名前はakiです。年齢は30歳です」
+
+// 従来の方法
+const message1 = "私の名前は" + name + "です。年齢は" + age + "です。";
+console.log(message1);
+
+// テンプレート文字列を用いた方法
+const message2 = `私の名前は${name}です。年齢は${age}です。`;
+console.log(message2);
